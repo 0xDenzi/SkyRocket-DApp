431e616490d4f79a91eb79f2a9af5f7802a6be31
abubakar4-u <abubakaryourkey309@gmail.com>
2024-05-20 16:02:12 +0500

    Issue: Navbar issue resolved
    On /launch page, its navbar appears on fund, and governance now. Which was not appearing at first. All changes being done in App.js (frontend). Navbar.js in component folder.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from "./pages/home";
 import Fund from "./pages/fund";
 import Gov from "./pages/gov";
 import './App.css'; // Import CSS file for styling
 import Bajwa from "./pages/Bajwa";
+import Navbar from "./components/Navbar"; // Import the Navbar component
 
 function App() {
   return (
     <BrowserRouter>
+      <ConditionalNavbar />
       <div className="app-container">
         <div className="content-wrapper">
           <Routes>
             {/* Define routes here */}
             <Route path="/" element={<Home />} /> {/* Home page route */}
-            <Route path="/launch" element={<Fund />} /> {/* Funding page route */}
-            <Route path="/fund" element={<Fund />} />
-            <Route path="/governance" element={<Gov />} />
+            <Route path="/launch" element={<Fund />} /> {/* Launch page route */}
+            <Route path="/fund" element={<Fund />} /> {/* Fund page route */}
+            <Route path="/governance" element={<Gov />} /> {/* Governance page route */}
             <Route path="/bajwa" element={<Bajwa />} />
             {/* Add other routes as necessary */}
           </Routes>
@@ -24,6 +26,17 @@ function App() {
       </div>
     </BrowserRouter>
   );
+}
+
+// Define a new component to conditionally render the Navbar
+const ConditionalNavbar = () => {
+  const location = useLocation();
+
+  // Define the routes where the Navbar should appear
+  const navbarRoutes = ['/launch', '/fund', '/governance'];
+
+  // Render the Navbar only on the specified routes
+  return navbarRoutes.includes(location.pathname) ? <Navbar /> : null;
 }
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,33 +2,31 @@
 
 import React from "react";
 import { Nav, NavLink, NavMenu, LaunchButton, NavLinkButton } from "./Navbar/NavbarElements";
-
-// import logo from '../images/a.jpg'; 
+import { Link } from 'react-router-dom'; // Use Link for client-side navigation
 
 const Navbar = () => {
-	return (
-		<>
-			<Nav style={{ display: "flex"}}>
-				<NavMenu style={{width: "100%", display:'flex', justifyContent:'space-between',}}>
-
-					<NavLink style={{marginRight: '100px', textDecoration: 'none'}} to="/home" activeStyle>
-						{/* <img src = {logo} style={{height: "50%", }}/> */}
-						{/* SkyRocket */}
-					</NavLink>
-					
-					<div style={{
-						display:"flex",
-						alignItems:"center",
-					}}>
-						<NavLinkButton to="/fund">Fund</NavLinkButton>
-						<NavLinkButton to="/governance">Governance</NavLinkButton>
-					
-					</div>
-						<LaunchButton style={{margin: '0px 60px 0 0'}}>Connect Wallet</LaunchButton>
-				</NavMenu>
-			</Nav>
-		</>
-	);
+  return (
+    <>
+      <Nav style={{ display: "flex" }}>
+        <NavMenu style={{ width: "100%", display: 'flex', justifyContent: 'space-between' }}>
+          <NavLink style={{ marginRight: '100px', textDecoration: 'none' }} to="/home" activeStyle>
+            {/* Uncomment the logo and text as needed */}
+            {/* <img src={logo} style={{ height: "50%" }} /> */}
+            {/* SkyRocket */}
+          </NavLink>
+          
+          <div style={{
+            display: "flex",
+            alignItems: "center",
+          }}>
+            <NavLinkButton to="/fund">Fund</NavLinkButton>
+            <NavLinkButton to="/governance">Governance</NavLinkButton>
+          </div>
+          <LaunchButton style={{ margin: '0px 60px 0 0' }}>Connect Wallet</LaunchButton>
+        </NavMenu>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;
